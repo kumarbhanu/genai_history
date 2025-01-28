ef69dd8ad35fd13fad2eacce281fd65e4970a892
bhanu <bhanu>
2025-01-28 18:39:00 +0530

    removed unused code

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 
 # Load environment variables
 load_dotenv()
-groq_api_key = os.getenv('GROQ_API_KEY')
-os.environ['HF_TOKEN'] = os.getenv('HF_TOKEN')
+# groq_api_key = os.getenv('GROQ_API_KEY')
+# os.environ['HF_TOKEN'] = os.getenv('HF_TOKEN')
 DATA_FILE = os.path.join(os.getcwd(), "data/eds_data.txt")
 
 groq_api_key = st.secrets["GROQ_API_KEY"]
